1a4d5edcde29816151f6ef131e3148d0a0a6275e
KPrasad07 <47730255+KPrasad07@users.noreply.github.com>
2019-02-22 09:31:53 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int kprasad(int *, int *);
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int b[5]={6,7,8,9,0};
+     kprasad(&a[0],&b[0]);
+    printf("Hiiii\n");
+    return 0;
+}
+
+int kprasad(int *p,int *q)
+{
+    int temp;
+    for(int i=0; i<5; i++)
+    {
+       temp=*q;
+       *p=*q;
+       *q=temp;
+       q++;
+       p++;
+
+    }
+    return 0;
+}
